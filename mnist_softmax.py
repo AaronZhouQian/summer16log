ec1fd3857283096eb8743332b699f284c9a1f6e8
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-07-18 15:34:46 -0700

    cpu

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -38,13 +38,16 @@ mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 sess = tf.InteractiveSession()
 
 # Create the model
-x = tf.placeholder(tf.float32, [None, 784])
+with tf.device("/cpu:0"):
+	x = tf.placeholder(tf.float32, [None, 784])
+	y_ = tf.placeholder(tf.float32, [None, 10])
+	
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 # Define loss and optimizer
-y_ = tf.placeholder(tf.float32, [None, 10])
+
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
 
