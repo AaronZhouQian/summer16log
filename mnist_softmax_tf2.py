0a5610d331c5bae5ffc68741d57e91ba93e119eb
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-07-18 16:37:54 -0700

    debug

--- mnist_softmax_tf2.py
+++ mnist_softmax_tf2.py
@@ -28,44 +28,54 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import timeit
 
-with tf.device('/gpu:0'):
-	flags = tf.app.flags
-	FLAGS = flags.FLAGS
-	flags.DEFINE_string('data_dir', 'data', 'Directory for storing data')
+BATCH_SIZE=100
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('data_dir', 'data', 'Directory for storing data')
 
-	mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-	batch_xs, batch_ys = mnist.train.next_batch(55000)
-	sess = tf.InteractiveSession()
+mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+batch_xs, batch_ys = mnist.train.next_batch(55000)
+sess = tf.Session()
 
-	# Create the model
-	x = tf.placeholder(tf.float32, [None, 784])
-	W = tf.Variable(tf.zeros([784, 10]))
-	b = tf.Variable(tf.zeros([10]))
-	y = tf.nn.softmax(tf.matmul(x, W) + b)
+# Create the model
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
 
-	# Define loss and optimizer
-	y_ = tf.placeholder(tf.float32, [None, 10])
+inputs=tf.get_variable("input",trainable=False, shape=[BATCH_SIZE, 784])
+labels = tf.get_variable("labels",trainable=False, shape=[BATCH_SIZE,10])
 
-	input=tf.get_variable(trainable=False, validate_shape=False, initializer=)
-	cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-	train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
 
-	start_time = timeit.default_timer()
-	# Train
-	tf.initialize_all_variables().run(feed_dict={x:batch_xs,y:batch_ys})
+softmax_probabilities = tf.nn.softmax(tf.matmul(inputs, W) + b)
 
-	for i in range(25000):
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(labels * tf.log(softmax_probabilities), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
 
-	    train_step.run({x: batch_xs, y_: batch_ys})
+start_time = timeit.default_timer()
+# Train
+tf.initialize_all_variables().run(feed_dict={x:batch_xs, y_:batch_ys})
 
-	    if i % 100 == 0:
-	    # Test trained model
-	    	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-	    	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-	    	print("%d iterations, accuracy: %f" % (i, accuracy.eval({x: mnist.test.images, y_: mnist.test.labels})))
+for j in range(50):
+	for i in range(0,50000,BATCH_SIZE):
+		# Test trained model
+		sess.run(inputs.assign(tf.slice(inputs, [i,0],[BATCH_SIZE,-1])))
+		sess.run(train_step)
+		correct_prediction = tf.equal(tf.argmax(softmax_probabilities, 1), tf.argmax(labels, 1))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-	end_time = timeit.default_timer()
-	print("final accuracy: %f" % accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
-	print("total time: %.1fs" % (end_time - start_time))
+end_time = timeit.default_timer()
+print("final accuracy: %f" % accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
+print("total time: %.1fs" % (end_time - start_time))
+
+
+
+
+
+
+
+
+
+
 
 
